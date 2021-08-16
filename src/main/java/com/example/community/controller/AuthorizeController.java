1c2f8c4e0a7cf8fe2e4c729a9b824e195c6b47bc
yuu1020 <940190259@qq.com>
2021-08-16 22:51:38 +0800

    init

--- src/main/java/com/example/community/controller/AuthorizeController.java
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -1,14 +1,31 @@
 package com.example.community.controller;
 
+import com.example.community.dto.AccessTokenDTo;
+import com.example.community.dto.GithubUser;
+import com.example.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,@RequestParam(name="state")String state )
     {
+        AccessTokenDTo accessTokenDTo = new AccessTokenDTo();
+        accessTokenDTo.setClient_id("15b62aa4afb8aea49628");
+        accessTokenDTo.setClient_secret("78a1acc482f5b5bb5eb0f687880d2788431ce9ec");
+        accessTokenDTo.setCode(code);
+        accessTokenDTo.setRedirect_uri("http://localhost:8887/callback");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTo);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getId());
+        System.out.println(user.getName());
         return "index";
     }
 }
---
+++ src/main/java/com/example/community/provider/GithubProvider.java
@@ -0,0 +1,50 @@
+package com.example.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.example.community.dto.AccessTokenDTo;
+import com.example.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTo accessTokenDTo)
+    {
+        MediaType mediaType= MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTo));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token=string.split("&")[0].split("=")[1];
+            return token;
+    } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public GithubUser getUser(String accessToken)
+    {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user")
+                .header("Authorization", "token " + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+}
