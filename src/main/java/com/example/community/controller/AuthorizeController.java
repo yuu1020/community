810562eca5cf27fe879b1056a50fe49246d9883a
yuu1020 <940190259@qq.com>
2021-08-17 16:17:13 +0800

    更新从配置文件映射变量

--- src/main/java/com/example/community/controller/AuthorizeController.java
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.example.community.dto.AccessTokenDTo;
 import com.example.community.dto.GithubUser;
 import com.example.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
-
+    @Value("${github.Client_id}")
+    private String client_Id;
+    @Value("${github.Client_secret}")
+    private String client_Secret;
+    @Value("${github.Redirect_uri}")
+    private String redirect_Uri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,@RequestParam(name="state")String state )
